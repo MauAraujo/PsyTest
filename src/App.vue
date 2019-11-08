bc71a78e921ac78e0680bb43a75252a2fc6bc217
Mauricio Araujo <mauricio.araujo@smartkode.mx>
2019-11-08 14:15:02 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <a-carousel arrows dotsClass="slick-dots slick-thumb">
+      <div slot="prevArrow" class="custom-slick-arrow" style="left: 10px;zIndex: 1">
+        <a-icon type="left-circle" />
+      </div>
+      <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px">
+        <a-icon type="right-circle" />
+      </div>
+
+      <div>
+        <a-card title="Default size card" style="width: 300px">
+          <a href="#" slot="extra">more</a>
+          <p>card content</p>
+          <p>card content</p>
+          <p>card content</p>
+        </a-card>
+      </div>
+      <div>
+        <a-card title="Default size card" style="width: 300px">
+          <a href="#" slot="extra">more</a>
+          <p>card content</p>
+          <p>card content</p>
+          <p>card content</p>
+        </a-card>
+      </div>
+      <div>
+        <a-card title="Default size card" style="width: 300px">
+          <a href="#" slot="extra">more</a>
+          <p>card content</p>
+          <p>card content</p>
+          <p>card content</p>
+        </a-card>
+      </div>
+      <div>
+        <a-card title="Default size card" style="width: 300px">
+          <a href="#" slot="extra">more</a>
+          <p>card content</p>
+          <p>card content</p>
+          <p>card content</p>
+        </a-card>
+      </div>
+    </a-carousel>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.ant-carousel >>> .slick-slide {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  line-height: 160px;
+  background: #e8f8f8;
+  overflow: hidden;
+}
+
+.ant-carousel >>> li {
+  background: rgb(0, 0, 0);
+}
+
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: rgb(0, 186, 186);
+  background-color: rgba(255, 255, 255, 0.11);
+  opacity: 0.6;
+}
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel >>> .custom-slick-arrow:hover {
+  color: rgba(0, 186, 186);
+  opacity: 1;
+}
+
+.ant-carousel >>> .slick-slide .ant-card {
+  color: rgb(0, 0, 0);
+  position: relative;
+  top: 20em;
+  left: 40%;
 }
 </style>
